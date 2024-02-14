d7276b28ebdafd4007c45a528e5758fc2ad28fc1
luckyhegde6 <60867180+luckyhegde6@users.noreply.github.com>
2024-02-15 03:29:38 +0530

    Adding the routes

--- React/finance-app/src/App.js
+++ React/finance-app/src/App.js
@@ -1,11 +1,54 @@
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import api from './api'
 
-function App() {
+const App = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [formData,setFormData] = useState({
+    amount : '',
+    category:'',
+    description: '',
+    is_income: false,
+    date: ''
+  });
+
+  const fetchTransaction = async () => {
+    const response = await api.get('/transactions');
+    setTransactions(response.data)
+  };
+
+  useEffect(() => {
+    fetchTransaction();
+  },[]);
+
+  const handleInputChange = (event) => {
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    setFormData({
+      ...formData,[event.target.name] : value
+    })
+  }
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    await api.post('/transactions/',formData);
+    fetchTransaction();
+    setFormData({
+      amount : '',
+      category:'',
+      description: '',
+      is_income: false,
+      date: ''
+    });
+  }
+
   return (
-    <div className="App">
-Test React App
+<div>
+  <nav className='navbar navbar-dark bg-primary'>
+    <div className='container-fluid'>
+      <a className='navbar-brand' href='#'>
+        Finance App
+      </a>
     </div>
-  );
+  </nav>
+</div>
+  )
 }
-
 export default App;
